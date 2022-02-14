2f6b6bedc181f3353f843c3734deced9b7d0fa78
Gustavo costa <guh-51@hotmail.com>
2022-02-14 12:49:12 -0300

    FileReader e BufferedReader

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -1,13 +1,45 @@
 package aplication;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Progam {
 
 	public static void main(String[] args) {
-		File file = new File("c:\\temp\\in.txt");
+		String path = "c:\\temp\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch(IOException e){
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			try {
+				if(fr != null){
+					fr.close();
+				}
+				if(br != null) {
+					br.close();
+				}
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+		/*File file = new File("c:\\temp\\in.txt");
 		Scanner sc = null;
 		try {
 			sc = new Scanner(file);
@@ -23,6 +55,5 @@ public class Progam {
 				sc.close();
 			}
 		}
-	}
-
+		*/
 }
