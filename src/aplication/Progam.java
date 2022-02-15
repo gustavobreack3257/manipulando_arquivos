31a6125bf01f906b2f8cee6370bbedca46d70729
Gustavo costa <guh-51@hotmail.com>
2022-02-15 08:16:44 -0300

    escrevendo arquivos

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -1,24 +1,33 @@
 package aplication;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Progam {
 
 	public static void main(String[] args) {
-		String path = "c:\\temp\\in.txt";		
+		String[] lines = new String[] { "Good morning", "Good afternoom", "Good nith"};
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			String line = br.readLine();
+		String path = "c:\\temp\\out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			path = br.readLine();
 			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
+			while(path != null) {
+				System.out.println(path);
+				br.readLine();
 			}
 		}
-		catch(IOException e){
-			System.out.println("Error: " + e.getMessage());
+		catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
