74277de76b525650a434080f9504a1e080ff2b43
Gustavo costa <guh-51@hotmail.com>
2022-02-15 09:25:41 -0300

    sub diretorio e pecorrendo pastas

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -1,33 +1,35 @@
 package aplication;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Progam {
 
 	public static void main(String[] args) {
-		String[] lines = new String[] { "Good morning", "Good afternoom", "Good nith"};
+		Scanner sc = new Scanner( System.in);
 		
-		String path = "c:\\temp\\out.txt";
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
 		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
-			for(String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-			BufferedReader br = new BufferedReader(new FileReader(path));
-			path = br.readLine();
-			
-			while(path != null) {
-				System.out.println(path);
-				br.readLine();
-			}
+		File path = new File(strPath);
+		// Percorre todas as pastas, dentro da pasta declarada.
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("Folders: ");
+		for(File folder : folders) {
+			System.out.println(folder);
 		}
-		catch(IOException e) {
-			e.printStackTrace();
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("Files: ");
+		for(File file : files) {
+			System.out.println(file);
 		}
+		// cria uma sub pasta
+		boolean sucess = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created sucess" + sucess);
+		
+		
+		sc.close();
 	}
+		
 }
